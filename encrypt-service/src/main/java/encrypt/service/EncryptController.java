254495fa07d6373a7f50ce2f5f20420f65d16bc3
Iván López <lopez.ivan@gmail.com>
2020-10-23 10:30:14 +0200

    Execute on IO event loop

--- encrypt-service/src/main/java/encrypt/service/EncryptController.java
+++ encrypt-service/src/main/java/encrypt/service/EncryptController.java
@@ -2,12 +2,16 @@ package encrypt.service;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
 
 @Controller
+@ExecuteOn(TaskExecutors.IO)
 public class EncryptController {
 
     @Get("/encrypt/{text}")
     public MyMessage encrypt(String text) {
+        System.out.println(Thread.currentThread().getName());
         var encrypted = new StringBuilder(text).reverse().toString();
         return new MyMessage(encrypted);
     }
