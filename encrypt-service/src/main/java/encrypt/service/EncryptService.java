39a2bad967363bc78614e3b08868873b4c86ffbb
Iván López <lopez.ivan@gmail.com>
2020-10-23 10:47:49 +0200

    Display the port on which encrypt-service is running

--- encrypt-service/src/main/java/encrypt/service/EncryptService.java
+++ encrypt-service/src/main/java/encrypt/service/EncryptService.java
@@ -1,5 +1,6 @@
 package encrypt.service;
 
+import io.micronaut.runtime.server.EmbeddedServer;
 import io.reactivex.Single;
 
 import javax.inject.Singleton;
@@ -7,8 +8,14 @@ import javax.inject.Singleton;
 @Singleton
 public class EncryptService {
 
+    private final EmbeddedServer embeddedServer;
+
+    public EncryptService(EmbeddedServer embeddedServer) {
+        this.embeddedServer = embeddedServer;
+    }
+
     public Single<MyMessage> encrypt(String text) {
         var encrypted = new StringBuilder(text).reverse().toString();
-        return Single.just(new MyMessage(encrypted));
+        return Single.just(new MyMessage(encrypted + " --> port: " + embeddedServer.getPort()));
     }
 }
